f37fa1f024c07de9e0fa9bcfa0acb9e939d2b847
liyuejiang <yuejiang_li@163.com>
2022-05-02 20:43:14 +0800

    Preprocess code for avazu and taobao

---
+++ custom/preprocess_h5_avazu_timeline.py
@@ -0,0 +1,32 @@
+import os
+import sys
+sys.path.append('../')
+import os
+from fuxictr import datasets
+from fuxictr.datasets.avazu import FeatureEncoder
+from fuxictr.utils import set_logger, print_to_json, load_dataset_config
+import logging
+
+if __name__ == '__main__':
+    # Load params from config files
+    config_dir = 'avazu_all_config'
+    dataset_id = 'avazu_pretrain_timeline'
+    params = load_dataset_config(config_dir, dataset_id)
+
+    # set up logger and random seed
+    set_logger(params, log_file='./demo.log')
+    logging.info(print_to_json(params))
+
+    # Set up feature encoder
+    feature_encoder = FeatureEncoder(feature_cols=params["feature_cols"],
+                                    label_col=params["label_col"],
+                                    dataset_id=dataset_id, 
+                                    data_root=params["data_root"])
+
+    datasets.build_pretrain_timeline_dataset(feature_encoder,
+                        dataset_name='avazu', 
+                        train_data=params["train_data"],
+                        pretrain_start_time=params['pretrain_start_time'],
+                        pretrain_end_time=params['pretrain_end_time'],
+                        timeline_start_time=params['timeline_start_time'],
+                        timeline_end_time=params['timeline_end_time'])
